f6a3320cc467c21f4692ede8a62db923361308ba
ferrodr <fernando.rguezm@gmail.com>
2024-09-20 14:53:37 +0200

    First commit with basic build and install docker.\n File upload is currently not working.

---
+++ lxcops.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+CONTAINER_NAME="myfirstcontainer"
+STORAGE_POOL="docker"
+
+install_docker() {
+	if sudo lxc storage list | grep -q "^| $STORAGE_POOL"; then
+		lxc storage volume delete $STORAGE_POOL $CONTAINER_NAME
+		lxc storage delete $STORAGE_POOL
+	fi
+	lxc storage create $STORAGE_POOL btrfs
+	lxc storage volume create $STORAGE_POOL $CONTAINER_NAME
+	lxc config device add $CONTAINER_NAME $STORAGE_POOL disk pool=$STORAGE_POOL source=$CONTAINER_NAME path=/var/lib/docker
+	lxc config set $CONTAINER_NAME security.nesting=true security.syscalls.intercept.mknod=true security.syscalls.intercept.setxattr=true
+	lxc restart $CONTAINER_NAME
+	lxc exec $CONTAINER_NAME -- bash -c "sudo apt-get update"
+	lxc exec $CONTAINER_NAME -- bash -c "sudo apt-get install \
+		ca-certificates \
+		curl \
+		gnupg \
+		lsb-release"
+	lxc exec $CONTAINER_NAME -- bash -c "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg \
+		--dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg"
+	lxc exec $CONTAINER_NAME -- bash -c 'echo \ 
+		"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+		| sudo tee /etc/apt/sources.list.d/docker.list > /dev/null'
+
+	lxc exec $CONTAINER_NAME -- bash -c 'echo \
+		"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+		| sudo tee /etc/apt/sources.list.d/docker.list > /dev/null'
+	lxc exec $CONTAINER_NAME -- bash -c "sudo apt-get update"
+	lxc exec $CONTAINER_NAME -- bash -c "sudo apt-get install -y docker-ce docker-ce-cli containerd.io"
+}
+
+restart_docker() {
+	lxc exec $CONTAINER_NAME -- bash -c 'for container in $(docker ps -q); do \
+		docker container stop $container; \
+		docker container rm $container; \       		
+		done'
+	# lxc exec $CONTAINER_NAME -- bash -c "docker image rm $DOCKER_NAME"
+	# lxc exec $CONTAINER_NAME -- bash -c "docker load -i /root/web/$DOCKER_FILE"
+	# lxc exec $CONTAINER_NAME -- bash -c "docker run -d $DOCKER_NAME"
+}
+
+load_file() {
+	lxc file push ./$1 $CONTAINER_NAME/root/web/
+}
+
+build () {
+	if lxc list | grep -q "^| $CONTAINER_NAME "; then
+		echo "Stopping and deleting current instance."
+		lxc stop $CONTAINER_NAME
+		lxc delete $CONTAINER_NAME
+	fi
+	lxc launch ubuntu:jammy $CONTAINER_NAME 
+	install_docker
+	restart_docker
+}
+
+print_help() {
+	echo "Operate the SUGUS web LXC container. This script MUST be run as root."
+	echo ""
+	echo "Commands:"
+    echo "  -n <name>  Sets the container name to <name>."
+	echo "  -b 	Builds the LXC container and overrides it in case it already exists. It also deploys the lxc container"
+	echo "  -d	Deploys the LXC container without cleaning the current container (that is, without overriding it)."
+	echo "  -l	Uploads the web's docker container file into the LXC container."
+	echo "  -r	Restarts the web's docker container inside the LXC container."
+	echo ""
+	echo "-l and -r options may be used together to reupload and reload the website's docker container. Order of flags matter."
+}
+
+while getopts "bdhrn:" arg; do
+	case $arg in
+		b)
+			echo "Building the LXC container. This will override the container in case it already exists..."
+			build	
+			;;
+		d)
+			echo "Deploying the LXC container..."
+			deploy
+			;;
+		r)
+			echo "Restarting the docker containers inside the LXC container."
+			restart_docker
+			;;
+		l)
+			echo "Uploading the $OPTARG file into the LXC container."
+			load_file
+			;;
+        n)
+            CONTAINER_NAME=$OPTARG
+            echo "Container name set to $OPTARG"
+            ;;
+		h)
+			print_help
+			;;
+		\?)
+			echo "Invalid option: -$OPTARG" >&2
+			print_help
+			;;
+	esac
+done
